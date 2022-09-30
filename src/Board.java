836d3f0feeb988713e705c7c6175faf73e471e74
claratoll <clara.toll@gmail.com>
2022-09-30 17:25:38 +0200

    Fixing game for base grade

--- src/Board.java
+++ src/Board.java
@@ -2,16 +2,45 @@ import java.util.ArrayList;
 
 public class Board {
 
+    private String tile;
+    private static int size;
 
     private String board;
 
     public Board(String board) {
-
         this.board = board;
     }
 
+    public Board (int size, String board){
+        this.size = size;
+        this.board = board;
+    }
+
+
+    public static void createBoard() {
+        //måste göra en reset här
+
+        //create board
+        for (int i = 0; i < size; i++) {
+          //  board.add(" ");
+        }
+    }
     //här ska allt gällande skapandet av boarden ligga
 
+    private static void printBoard(boolean keepPlaying) {
+        if (keepPlaying) {
+            System.out.println("Current board:");
+        } else {
+            System.out.println("WINNER BOARD");
+        }
+
+
+       // System.out.println(" " + board.get(0) + " | " + board.get(1) + " | " + board.get(2));
+       // System.out.println("---+---+---");
+       // System.out.println(" " + board.get(3) + " | " + board.get(4) + " | " + board.get(5));
+       // System.out.println("---+---+---");
+       // System.out.println(" " + board.get(6) + " | " + board.get(7) + " | " + board.get(8));
+    }
 
 }
 
---
+++ src/Game.java
@@ -0,0 +1,212 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+enum Turn {
+    Player,
+    Player2,
+}
+
+public class Game {
+
+    private static ArrayList<Player> players = new ArrayList<Player>();
+    private static ArrayList<String> board = new ArrayList<String>();
+
+
+    private Turn turn = null;
+
+    Boolean startGame = true;
+    Boolean booleanPlayGame = true;
+    Boolean keepPlaying = true;
+
+
+
+    public Game() {
+        createPlayers();
+
+        while (keepPlaying) {
+            if (startGame) {
+                createBoard();
+                startGame = false;
+            }
+
+            printBoard(booleanPlayGame);
+
+            turn = Turn.values()[new Random().nextInt(Turn.values().length)];
+
+            //start the game
+            Play();
+
+            System.out.println("Do you want to play again? yes or no");
+            Scanner sc = new Scanner(System.in);
+            try{
+                String answer = sc.nextLine();
+                if (answer.equals("yes")) {
+                    booleanPlayGame = true;
+
+                    board.clear();
+                    createBoard();
+                    Play();
+
+                } else {
+                    System.out.println("Thank you for playing!");
+                    keepPlaying = false;
+                    //    System.out.println(player.getName() + " won!");
+                }
+            } catch (Exception e){
+                System.out.println("Please write 'yes' or 'no'");
+            }
+
+        }
+    }
+
+    public static ArrayList<Player> createPlayers() {
+        //create players
+        Scanner sc = new Scanner(System.in);
+        int playerNum = 2;
+
+        for (int i = 0; i < playerNum; i++) {
+            System.out.println("Player " + (i + 1) + ", please write your name");
+            String name = sc.nextLine();
+            System.out.println(name + ", please write your letter you want to play with");
+            String letter = sc.nextLine();
+            players.add(new Player(name, letter, i));
+        }
+        return players;
+
+    }
+
+    private static void createBoard() {
+      //  try {
+       //     Scanner sc = new Scanner(System.in);
+        //    System.out.println("How many tiles do you want to play with");
+       //     int size = sc.nextInt();
+
+          //  board.add(size, " ");
+
+     //   }catch (Exception e){
+       //     System.out.println("Invalid input; try again.");
+     //   }
+
+        for (int i = 0; i < 10; i++) {
+            board.add(" ");
+        }
+    }
+
+
+    public void Play() {
+        while (booleanPlayGame) {
+
+            Player player = turn == Turn.Player ? players.get(0) : players.get(1);
+
+
+            // while true - loopa spelarna
+            try {
+                System.out.println(player.getName() + " choose a spot - write a number between 1 and 9");
+                Scanner sc = new Scanner(System.in);
+                int inputNumber = sc.nextInt();
+
+                if (inputNumber < 1 || inputNumber > 9) {
+                    System.out.println("Invalid input; re-enter your number");
+                    inputNumber = sc.nextInt();
+                }
+
+                if (1 == inputNumber) {
+                    board.set(0, player.getLetter());
+                } else if (2 == inputNumber) {
+                    board.set(1, player.getLetter());
+                } else if (3 == inputNumber) {
+                    board.set(2, player.getLetter());
+                } else if (4 == inputNumber) {
+                    board.set(3, player.getLetter());
+                } else if (5 == inputNumber) {
+                    board.set(4, player.getLetter());
+                } else if (6 == inputNumber) {
+                    board.set(5, player.getLetter());
+                } else if (7 == inputNumber) {
+                    board.set(6, player.getLetter());
+                } else if (8 == inputNumber) {
+                    board.set(7, player.getLetter());
+                } else if (9 == inputNumber) {
+                    board.set(8, player.getLetter());
+                }
+
+                checkIfWinner(player.getLetter());
+                printBoard(booleanPlayGame);
+
+
+                turn = player.getId() == 0 ? Turn.Player2 : Turn.Player;
+
+
+     /*   if (player.getId() == 0) {
+            turn = Turn.Player2;
+        } else {
+            turn = Turn.Player;
+        }*/
+
+
+            } catch (Exception e) {
+                System.out.println("Invalid input; try again.");
+            }
+        }
+
+    }
+
+    private void checkIfWinner(String letter) {
+
+        for (int a = 0; a < 8; a++) {
+            String winnerLine = null;
+
+            switch (a) {
+                case 0:
+                    winnerLine = board.get(0) + board.get(1) + board.get(2);
+                    break;
+                case 1:
+                    winnerLine = board.get(3) + board.get(4) + board.get(5);
+                    break;
+                case 2:
+                    winnerLine = board.get(6) + board.get(7) + board.get(8);
+                    break;
+                case 3:
+                    winnerLine = board.get(0) + board.get(3) + board.get(6);
+                    break;
+                case 4:
+                    winnerLine = board.get(1) + board.get(4) + board.get(7);
+                    break;
+                case 5:
+                    winnerLine = board.get(2) + board.get(5) + board.get(8);
+                    break;
+                case 6:
+                    winnerLine = board.get(0) + board.get(4) + board.get(8);
+                    break;
+                case 7:
+                    winnerLine = board.get(2) + board.get(4) + board.get(6);
+                    break;
+            }
+
+            if (winnerLine.equals(letter + letter + letter)) {
+                System.out.println("WE HAVE A WINNER! ");
+                //     player.increaseScore();
+                booleanPlayGame = false;
+                break;
+            }
+        }
+    }
+
+    private static void printBoard(boolean keepPlaying) {
+        if (keepPlaying) {
+            System.out.println("Current board:");
+        } else {
+            System.out.println("WINNER BOARD");
+        }
+
+        System.out.println(" " + board.get(0) + " | " + board.get(1) + " | " + board.get(2));
+        System.out.println("---+---+---");
+        System.out.println(" " + board.get(3) + " | " + board.get(4) + " | " + board.get(5));
+        System.out.println("---+---+---");
+        System.out.println(" " + board.get(6) + " | " + board.get(7) + " | " + board.get(8));
+    }
+
+
+}
